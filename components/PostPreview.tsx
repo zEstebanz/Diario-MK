6ac9695ab54ef843a29ba099c3716ab185741c68
Esteban <estebanoller73@gmail.com>
2024-01-06 03:51:08 -0300

    implementacion de hover:

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -5,22 +5,23 @@ import Image from "next/image";
 const PostPreview = (props: PostMetadata) => {
   return (
     <div
-      className="border border-slate-300 p-4 rounded-md shadow-sm
-    bg-white"
+      className="border border-slate-400 p-4 rounded-md shadow-sm
+      bg-white post-preview"
     >
-      <p className="text-sm text-slate-400">{props.date}</p>
+      <p className="text-sm text-slate-600">{props.date}</p>
 
       <Link href={`/posts/${props.slug}`}>
-        <h2 className=" text-violet-600 hover:underline mb-4">{props.title}</h2>
+        <h2 className="text-black font-bold mb-4">{props.title}</h2>
         <div className="relative h-48 w-full mb-4">
           <Image
             src={String(props.img)}
             alt="img"
             layout="fill"
-            objectFit="cover"  // Puedes cambiar a "contain" si prefieres
+            objectFit="cover"
           />
         </div>
       </Link>
+
       <p className="text-slate-700">{props.subtitle}</p>
     </div>
   );
