c8eec4158db6fb5fd9d3c6927c81ee0ad70e125a
oller.esteban <estebanoller73@gmail.com>
2024-04-02 14:59:06 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -13,16 +13,12 @@ const nextConfig = {
             'miro.medium.com'
         ],
     },
-    "headers": [
-        {
-            "source": "http://hn.algolia.com/api/v1/search?tags=front_page",
-            "headers": [
-                { "key": "Access-Control-Allow-Credentials", "value": "true" },
-                { "key": "Access-Control-Allow-Origin", "value": "*" },
-                { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-                { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-            ]
-        }
+    source: "/api/:path*",
+    headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
     ]
 }
 
