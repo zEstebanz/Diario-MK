5bd46828d7c76044ffd84ee9ff7a60c804d0ac53
oller.esteban <estebanoller73@gmail.com>
2024-04-02 16:02:56 -0300

    update config axios

--- app/utils/services/axiosConfig.js
+++ app/utils/services/axiosConfig.js
@@ -5,6 +5,6 @@ const angoliaApi = axios.create({
     baseURL: API,
 });
 
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'https://diario-mk.vercel.app';
 
 export default angoliaApi;
