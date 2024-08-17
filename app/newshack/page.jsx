82c902768b1877faed950a318439b7c08f5f4b82
Esteban <estebanoller73@gmail.com>
2024-08-16 21:56:43 -0300

    implementacion de noticias

--- app/newshack/page.jsx
+++ app/newshack/page.jsx
@@ -21,25 +21,35 @@ function Page() {
   }, []);
 
   return (
-    <section className='min-h-screen h-auto bg-black section-newshack'>
+    <section className='min-h-screen h-auto bg-black section-newshack py-10'>
 
       <div className="port pt-[150px] pb-[150px]">
         <h1 className="title text-center text-6xl md:text-8xl text-white">News Hack</h1>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 p-[5%]">
-        {news.map(item => (
-          <div key={item.objectID} className="news-item border border-gray-400 rounded-md p-4 bg-white shadow-md"><a href={item.url} target="_blank" rel="noopener noreferrer" className="text-black font-bold mb-4 block">
-            {item.title}
-          </a>
-            {item.image && <img src={item.image} alt={item.title} className="w-full h-32 object-cover mb-4" />}
-            <a href={item.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline block">
-              Read more
-            </a>
-          </div>
-        ))
-        }
-      </div >
+      <div className="container mx-auto px-4">
+        <h1 className="text-center text-4xl md:text-6xl font-bold mb-12">Daily News</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {news.map(item => (
+            <div key={item.objectID} className="news-item bg-white border border-gray-300 rounded-lg overflow-hidden shadow-lg">
+              <a href={item.url} target="_blank" rel="noopener noreferrer" className="block">
+                <div className="p-4">
+                  <h2 className="text-lg font-semibold text-gray-800 hover:text-blue-600 transition duration-300">
+                    {item.title}
+                  </h2>
+                  <p className="text-sm text-gray-500 mt-2">
+                    By <span className="font-medium text-gray-700">{item.author}</span> on {new Date(item.created_at).toLocaleDateString()}
+                  </p>
+                  <p className="text-sm text-gray-500 mt-2">
+                    Points: {item.points} | Comments: {item.num_comments}
+                  </p>
+                </div>
+                {item.image && <img src={item.image} alt={item.title} className="w-full h-48 object-cover" />}
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
 
     </section >
 
--- app/utils/services/axiosConfig.js
+++ app/utils/services/axiosConfig.js
@@ -1,5 +1,5 @@
-import axios from"axios";
-import { API } from"./constants";
+import axios from "axios";
+import { API } from "./constants";
 
 const angoliaApi = axios.create({
     baseURL: 'https://hn.algolia.com/api/v1/',  // Asegúrate de usar HTTPS
